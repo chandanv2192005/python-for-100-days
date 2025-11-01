fffe970fc8071dba386c366ed3c39a38f906469f
chandan <chandanv2192005@gmail.com>
2025-11-01 22:41:48 +0530

    Add Day 3: Control Flow examples

---
+++ Day3/day3_control_flow.py
@@ -0,0 +1,17 @@
+# Day 3: Control Flow
+# If-Else Statements
+number = 10
+if number > 0:
+    print("The number is positive.")
+else:
+    print("The number is not positive.")
+
+# For Loop
+for i in range(5):
+    print("Iteration:", i)
+
+# While Loop
+count = 0
+while count < 3:
+    print("Count:", count)
+    count += 1
