b2168f2b56d3f4f13301274d9a1692f681fc1cec
chandan <chandanv2192005@gmail.com>
2025-11-01 22:40:11 +0530

    Add Day 2: Variables and Data Types

---
+++ Day2/day2_variables.py
@@ -0,0 +1,10 @@
+# Day 2: Variables and Data Types
+# Variables
+name = "Chandan"
+age = 25
+is_learning = True
+
+# Print variables
+print("Name:", name)
+print("Age:", age)
+print("Is learning Python:", is_learning)
