6644eb1cd211d1134dd9806a8ffa9d067a64c24a
chandan <chandanv2192005@gmail.com>
2026-02-25 05:53:20 +0530

    Add json.dumps example

---
+++ json.dumps().py
@@ -0,0 +1,18 @@
+#AND OPERATOR
+A and B
+True and True -> True
+True and False -> False
+False and True -> False
+False and False -> False
+
+#OR OPERATOR
+A or B
+True or True -> True
+True or False -> True
+False or True -> True
+False or False -> False
+
+#NOT OPERATOR
+not A
+not True -> False
+not False -> True
