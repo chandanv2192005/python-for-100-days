73d0d41be27127a786261cae4f2443399e36c04f
chandan <chandanv2192005@gmail.com>
2026-03-10 19:35:00 +0530

    Add array creation example

---
+++ Arrays/01_array_creation.py
@@ -0,0 +1,11 @@
+import array
+
+# Create an array of integers (type code 'i')
+int_array = array.array('i', [10, 20, 30, 40, 50])
+
+print("Array elements:")
+for el in int_array:
+    print(el)
+
+print(f"First element: {int_array[0]}")
+print(f"Last element: {int_array[-1]}")
