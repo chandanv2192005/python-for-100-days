416e07be964596de7d6c31257d0c7c4bf37aadaa
V Chandan <chandanv2192005@gmail.com>
2026-03-16 22:00:23 +0530

    Update 01_array_creation.py

--- Arrays/01_array_creation.py
+++ Arrays/01_array_creation.py
@@ -6,7 +6,7 @@ int_array = array.array('i', [10, 20, 30, 40, 50])
 print("Array elements:")
 for el in int_array:
     print(el)
-
+ 
 print(f"First element: {int_array[0]}")
 print(f"Last element: {int_array[-1]}")
  
