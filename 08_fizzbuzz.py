ad09322bfbe34ca381dbc1ebcfde78bea2735b78
V Chandan <chandanv2192005@gmail.com>
2026-04-01 21:38:43 +0530

    Update fizzbuzz.py docstring formatting

--- 08_fizzbuzz.py
+++ 08_fizzbuzz.py
@@ -4,7 +4,7 @@ Logic: Print numbers from 1 to n.
 For multiples of 3 print "Fizz" instead of the number.
 For multiples of 5 print "Buzz".
 For numbers which are multiples of both 3 and 5 print "FizzBuzz".
-"""
+""" 
 
 def fizzbuzz(n):
     for i in range(1, n + 1):
