55c352ce2e5d51fe4c590b79a630c36a1eb517d2
V Chandan <chandanv2192005@gmail.com>
2026-04-01 21:38:50 +0530

    Update 08_fizzbuzz.py

--- 08_fizzbuzz.py
+++ 08_fizzbuzz.py
@@ -3,7 +3,7 @@
 Logic: Print numbers from 1 to n. 
 For multiples of 3 print "Fizz" instead of the number.
 For multiples of 5 print "Buzz".
-For numbers which are multiples of both 3 and 5 print "FizzBuzz".
+For numbers which are multiples of both 3 and 5 print "FizzBuzz". 
 """ 
 
 def fizzbuzz(n):
