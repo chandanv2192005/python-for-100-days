44db06b8bb2adbc1eaab627cdabb102a0f1e5732
V Chandan <chandanv2192005@gmail.com>
2026-04-03 22:02:03 +0530

    Fix formatting in greet function calls

--- functions/04_default_parameters.py
+++ functions/04_default_parameters.py
@@ -3,5 +3,5 @@ def greet(name, message="Good morning"):
     print(f"{message}, {name}!")
 
 greet("John") # Good morning, John! 
-greet("Sarah", "Good evening") # Good evening, Sarah!
+greet("Sarah", "Good evening") # Good evening, Sarah! 
 greet("Alex", "Happy birthday!") # Happy birthday!, Alex!
